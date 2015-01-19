14bd93c0be7a84cf88eaf20df49a6821c03c3a65
Erick Bett <erickeno@yahoo.com>
2015-01-19 16:46:45 -0500

    make a User model with password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116233446) do
+ActiveRecord::Schema.define(version: 20150119212635) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title"
@@ -29,5 +29,13 @@ ActiveRecord::Schema.define(version: 20150116233446) do
   end
 
   add_index "pins", ["board_id"], name: "index_pins_on_board_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
